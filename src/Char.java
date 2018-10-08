14034c2c7932b13de2a1993fe7d571d6bc454fd5
Wesam Herbawi <wesam.herbawi@gmail.com>
2018-10-08 16:18:11 +0200

    char lecture

--- src/Char.java
+++ src/Char.java
@@ -1,13 +1,31 @@
+import java.util.Scanner;
 
 public class Char {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("hello\\ \"d\" ");
-		char c = 'A';
-		int n = c;
+		char x = 'a';
+		int w = x;
+		char z = (char) (w + 1);
+		char c='d';
 		System.out.println(++c);
+		System.out.println(x);
+		System.out.println(w);
+		System.out.println(z);
+		System.out.println((int) z);
 
+		int userInput = 'D';
+		if ((userInput >= 'a' && userInput <= 'z') || (userInput >= 'A' && userInput <= 'Z'))
+			System.out.println("is a letter");
+		else
+			System.out.println("not eng letter");
+		Scanner s = new Scanner(System.in);
+		System.out.println("please enter a letter");
+		char input2 = s.next().charAt(0);
+		if (input2 >= 'a' && input2 <= 'z')
+			System.out.println("small " +input2);
+		
+		System.out.println(Character.isLowerCase('a'));
 	}
+
 
 }
