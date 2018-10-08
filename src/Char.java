08e408658e8e9ed00963c35b836b9880ce7797c1
Wesam Herbawi <wesam.herbawi@gmail.com>
2018-10-08 16:13:41 +0200

    char lecture

---
+++ src/Char.java
@@ -0,0 +1,13 @@
+
+public class Char {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello\\ \"d\" ");
+		char c = 'A';
+		int n = c;
+		System.out.println(++c);
+
+	}
+
+}
